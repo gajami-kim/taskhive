706a67d583a32802e714f1bdb2c12e9810afd9ae
gajami-kim <qansd38@gmail.com>
2024-12-08 14:04:59 +0900

    include frontend as part of the main repository

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <section className='main'>
+        
+      </section>
+    </>
+  );
+}
+
+export default App;
